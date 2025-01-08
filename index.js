5bb509ae8d5cc91f58d44e54391eea65be6e1a0d
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-01-08 17:43:19 -0600

    passes test

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    const cards =[];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return cards;
+}
+
+writeCards(names[i]);
+
+
+let number = 10;
+function countDown(number){
+    let i = number;
+    while (i > -1) {
+        console.log(i);
+        i--;
+    }
+    return number;
+}
+countDown(number);
